dc347301079d53fe21ae000e3d06e11fa0ae2cf6
xiaolijiushishuai <836075337@qq.com>
2023-05-11 23:07:48 +0800

    update by lian

--- weibo/fb_test.py
+++ weibo/fb_test.py
@@ -4,7 +4,6 @@ import json
 import requests
 
 
-
 account_id = '36'
 
 headers = {"sec-ch-ua": "", "x-xsrf-token": "b72af5", "sec-ch-ua-mobile": "?0", "user-agent": "Mozilla/5.0 (Linux; Android 8.0.0; Nexus 5X Build/OPR4.170623.006) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.63 Mobile Safari/537.36", "accept": "application/json, text/plain, */*", "mweibo-pwa": "1", "x-requested-with": "XMLHttpRequest", "sec-ch-ua-platform": "Windows", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://m.weibo.cn/compose/", "accept-encoding": "gzip, deflate, br", "cookie": "XSRF-TOKEN=b72af5; _T_WM=73920313007; SUB=_2A25JWIHlDeRhGeNL61oY8yfFzzuIHXVqoi-trDV6PUJbktAGLROkkW1NSTlhS5yDtxHtQk5Xge8Nr0WJXH13fb4Q; WEIBOCN_FROM=1110006030; MLOGIN=1; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWef-c.oDMGP.TaUWJPnSJs5NHD95QfSK5R1Ke41KBNWs4DqcjwB-_Wi--Ni-2Xi-i8i--Xi-ihiKLWi--NiKy8iKyF; SSOLoginState=1683812789; M_WEIBOCN_PARAMS=luicode%3D20000174%26uicode%3D20000174"}
